71ec11b7846fd66239d1c683be80d649a72c2020
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-18 18:04:40 +0100

    add test with variables :arrow_right: main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
@@ -7,5 +7,18 @@ int main()
     cout << "Hello World" << endl << "this is a test" << endl;
     cout << "test avec les \'guillemets\' " << endl;
     cout << "un autre test \\/O\\/ " << endl;
+
+    string phrase("that's working");
+    cout << phrase << endl;
+
+    int number(120);
+    cout << number << endl;
+
+    double pi(3.1415);
+    cout << pi << endl;
+
+
+    int firstNumber(60), secondNumber(40);
+    cout << firstNumber + secondNumber << endl; 
     return 0;
 }
