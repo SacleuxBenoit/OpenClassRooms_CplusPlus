c0a62da029c2218cb90bd9432306b3396bd2adb7
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-18 17:40:44 +0100

    add test with backslash and quot marks :arrow_right: main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    cout << "Hello World" << endl << "this is a test" << endl;
+    cout << "test avec les \'guillemets\' " << endl;
+    cout << "un autre test \\/O\\/ " << endl;
     return 0;
 }
