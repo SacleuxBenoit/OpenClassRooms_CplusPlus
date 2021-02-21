4ff8ae761ff9af3c77a4ea0f266624d26c32c8a0
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-21 17:03:37 +0100

    calculate the numbers of second with given numbers :arrow_right: otherTest.cpp

---
+++ otherTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int nombreDeSecondes(int heures, int minutes, int secondes);
+
+int main(){
+    cout << nombreDeSecondes(1,10,25) << endl;
+
+    return 0;
+}
+
+int nombreDeSecondes(int heures, int minutes, int secondes){
+    int total = 0;
+
+    total = heures * 60 * 60;
+    total += minutes * 60;
+    total += secondes;
+
+    return total;
+}
